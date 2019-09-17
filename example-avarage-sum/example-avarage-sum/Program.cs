21118b2a99ac5fcfb21edce6b96ebc8ae880d7cc
Aleksi Mäkelä <aleksi.makela@student.saimia.fi>
2019-09-17 11:54:09 +0300

    average example version 1 done

--- example-avarage-sum/example-avarage-sum/Program.cs
+++ example-avarage-sum/example-avarage-sum/Program.cs
@@ -6,7 +6,30 @@ namespace example_avarage_sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int summa = 0;
+            int i = 1;
+            Console.Write("Ohjelma lastee keskiarvon syöttämien lukujesi perusteella. " +
+                "Kuinka monta lukua syötät? ");
+            int lkm = int.Parse(Console.ReadLine());
+            while (i <= lkm)
+            {
+                int x = int.Parse(Console.ReadLine());
+                summa = summa + x;
+                i = i + 1;
+            }
+
+            if (lkm > 0)
+            {
+                double ka = summa / lkm;
+                Console.WriteLine("Lukujen keskiarvo on: " +ka);
+            }
+            else
+            {
+                Console.WriteLine("Lukuja ei ole syötetty!");
+            }
+
+            
+
         }
     }
 }
