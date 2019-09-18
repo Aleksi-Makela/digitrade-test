fb86c3c90bc8d3730b4ab27cd9166fa581b418a2
Aleksi Mäkelä <aleksi.makela@student.saimia.fi>
2019-09-18 15:25:44 +0300

    factorial example first version

--- factorial-example/factorial-example/Program.cs
+++ factorial-example/factorial-example/Program.cs
@@ -6,7 +6,30 @@ namespace factorial_example
     {
         static void Main(string[] args)
         {
+            int userNumber = 0;
+            int fact = 1;
+
             Console.WriteLine("Kertoman laskenta");
+            Console.Write("Syötä luku, jonka kertoma lasketaan: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber < 0)
+            {
+                Console.WriteLine("Virheellinen syöte.");
+            }
+            else
+            {
+                while(userNumber > 0)
+                {
+                    fact = fact * userNumber;
+                    userNumber = userNumber - 1;
+                }
+                Console.WriteLine("Vastaus:" + fact);
+            }
+
+
+
+
         }
     }
 }
